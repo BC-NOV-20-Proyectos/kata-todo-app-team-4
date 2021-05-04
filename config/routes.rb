29a66a0094dcda1e0579122a98e115027b7657f4
MiguelOcegueraM <migueloceguera.m@gmail.com>
2021-05-04 17:45:25 -0500

    Added the ability to create a todo list and also items in the list and delete them
    Co-authored-by: OchoaHub <jochoa2@ucol.mx>

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :todo_lists do
+    resources :todo_items
+  end
+
+  
+  root "todo_lists#index" #after this test we're going to change this root for the login
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
